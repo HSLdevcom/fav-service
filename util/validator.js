29db26b939384c86c661f4545ba38c8a5a8986ff
Joonas Rissanen <joonas.rissanen@cgi.com>
2021-05-04 11:17:44 +0300

    chore: update packages and add ajv-formats to validator

--- util/validator.js
+++ util/validator.js
@@ -9,11 +9,14 @@ var _Err = _interopRequireDefault(require("./Err"));
 
 var _ajv = _interopRequireDefault(require("ajv"));
 
+var _ajvFormats = _interopRequireDefault(require("ajv-formats"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const ajv = new _ajv.default({
   allErrors: true
 });
+(0, _ajvFormats.default)(ajv);
 
 function validate(schema, data) {
   const validate = ajv.compile(schema);
