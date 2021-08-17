182be3da2d1bc5b222a44845a22402135d310b66
Joonas Rissanen <joonas.rissanen@cgi.com>
2021-08-17 10:02:37 +0300

    feat: add pre-commit lint

--- src/getFavourites/index.ts
+++ src/getFavourites/index.ts
@@ -24,7 +24,7 @@ const getSchema: JSONSchemaType<GetSchema> = {
       type: 'string',
     },
   },
-  required: ['hslId', 'store'],
+  required: ['hslId', 'store'], 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
 } as any;
 
