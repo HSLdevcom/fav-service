1dfd94067b11acc7fd649069294124126f1f88ae
Joonas Rissanen <joonas.rissanen@cgi.com>
2021-08-24 14:54:17 +0300

    test: add get favourites unit tests

---
+++ src/getFavourites/getFavourites.test.ts
@@ -0,0 +1,85 @@
+import { Context } from '@azure/functions';
+import * as nock from 'nock';
+import getFavourites from '.';
+import * as mockData from '../get_mock.json';
+
+const dataStorageNotFoundResponse = {
+  message: 'Datastorage not found',
+};
+
+const dataStorageFoundResponse = {
+  resources: [
+    {
+      id: 'fafa',
+    },
+  ],
+};
+
+const dataStorageCreateResponse = {
+  id: '5c75387aa6734c425e86b092',
+  name: 'string',
+  description: 'string',
+  ownerId: 'undefined',
+  adminAccess: ['999'],
+  readAccess: ['999'],
+  writeAccess: ['999'],
+};
+
+describe('getFavourites', () => {
+  let context: Context;
+
+  beforeEach(() => {
+    context = { log: jest.fn() } as unknown as Context;
+    process.env.hslIdUrl = 'http://localhost';
+    process.env.clientId = '999';
+    process.env.clientCredentials = 'Basic asdasd';
+    process.env.redisHost = 'redis';
+    process.env.redisPass = '';
+    process.env.redisPort = '6379';
+  });
+
+  afterEach(() => nock.cleanAll());
+
+  it('should return empty array when datastorage is not found or created yet', async () => {
+    nock('http://localhost')
+      .get('/api/rest/v1/datastorage')
+      .query({ dsfilter: `ownerId eq "foobar" and name eq "favorites-999"` })
+      .reply(404, dataStorageNotFoundResponse);
+
+    const request = {
+      method: 'GET',
+      params: {
+        id: 'foobar',
+      },
+      query: {
+        store: 'fav',
+      },
+    };
+    await getFavourites(context, request);
+    expect(context?.res?.status).toEqual(200);
+    expect(context?.res?.body).toEqual('[]');
+  });
+
+  it('should return favourites with existing hslid', async () => {
+    nock('http://localhost')
+      .get('/api/rest/v1/datastorage')
+      .query({ dsfilter: `ownerId eq "foobar" and name eq "favorites-999"` })
+      .reply(200, dataStorageFoundResponse);
+
+    nock('http://localhost')
+      .get('/api/rest/v1/datastorage/fafa/data')
+      .reply(200, mockData);
+
+    const request = {
+      method: 'GET',
+      params: {
+        id: 'foobar',
+      },
+      query: {
+        store: 'fav',
+      },
+    };
+    await getFavourites(context, request);
+    expect(JSON.parse(context?.res?.body)).toEqual(mockData);
+  });
+});
