b175651d3b0eb25fb63aeac9ebf25d5901311c10
Janne Antikainen <antiik91@gmail.com>
2024-02-29 10:47:03 +0200

    cleanup

--- jest.setup.redis-mock.js
+++ jest.setup.redis-mock.js
@@ -3,8 +3,8 @@ const RedisMock = require('ioredis-mock');
 jest.mock('ioredis', () => jest.requireActual('ioredis-mock'));
 
 const redis = new RedisMock({
-  port: 1234, // Replace with the actual port value
-  host: 'example.com', // Replace with the actual host value
+  port: 1234,
+  host: 'example.com',
   data: {
     'Key:1': [JSON.stringify({ state: 'INITIAL', recordingError: null })],
     'Key:2': [JSON.stringify({ JSONVersionId: '1.0', agentTack: '11ebd1d4' })],
