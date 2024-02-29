726a441517d6951feb1262b25b069e1735189799
Janne Antikainen <antiik91@gmail.com>
2024-02-29 10:46:50 +0200

    redis mock working again

--- jest.setup.redis-mock.js
+++ jest.setup.redis-mock.js
@@ -1,1 +1,12 @@
-jest.mock('ioredis', () => jest.requireActual('ioredis-mock/jest'));
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const RedisMock = require('ioredis-mock');
+jest.mock('ioredis', () => jest.requireActual('ioredis-mock'));
+
+const redis = new RedisMock({
+  port: 1234, // Replace with the actual port value
+  host: 'example.com', // Replace with the actual host value
+  data: {
+    'Key:1': [JSON.stringify({ state: 'INITIAL', recordingError: null })],
+    'Key:2': [JSON.stringify({ JSONVersionId: '1.0', agentTack: '11ebd1d4' })],
+  },
+});
