6d8af29ba5d9d70465fdf4654ee0562d1f58e9e0
Vesa Meskanen <vesa.meskanen@cgi.com>
2024-03-06 15:36:45 +0200

    Fix typo

--- agent/Agent.ts
+++ agent/Agent.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
-kyimport axios, { AxiosResponse } from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { Context } from '@azure/functions';
 import {
   getHslIdUrl,
